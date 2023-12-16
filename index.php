834b05ff20917def18102d438c19b7d302484704
saad <saadk9666@gmail.com>
2023-12-16 16:10:36 +0500

    front end completed of TODOS

--- index.php
+++ index.php
@@ -4,8 +4,31 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>To Do List</title>
+    <link rel="stylesheet" href="CSS/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
 <body>
+  <div class="container">
+    <div class="div-1">
+      <h2>TODO LIST by Engineer Saad Ali</h2>
+      <div class="row input-1"> <br>
+      <input type="text" name="" class="form-control">
+      <button>Add it </button>
+      </div>
+    </div><br>
+    <div class=" row div-2">
+        <div class="col data">
+            <h3>Coding </h3>
+            <p>This is Awesome</p>
+        </div>
+        <div class="col  buttons">
+        <button class="btn btn-success">Success</button>
+        <button class="btn btn-danger">Delete</button>
+        </div>
+
+    </div>
+  </div>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
     
 </body>
 </html>
